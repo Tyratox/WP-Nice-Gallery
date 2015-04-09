c98948775e07ee0517ed15e80501b6559b4ed627
Nico Hauser <me@tyratox.ch>
2015-04-09 13:29:57 +0200

    INIT

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== WP Nice gallery ===
+Version: 1.1
+
+Requires at least: 3.9.1
+Tested up to: 4.1.1
+Stable tag: 4.1.1
+
+Replaces the default gallery function with a nicer one.
+
+
+
+== Description ==
+Description:
+
+Replaces the default gallery function with a nicer one.
+WP Implementation of http://osvaldas.info/image-lightbox-responsive-touch-friendly.
+
+Support: me@tyratox.ch
+
+
+
+== Changelog ==
+
+= 1.0 =
+* Created
+
+= 1.1 =
+* Bug fixes & multiple galleries per page support
+
+== Installation ==
+
+1. Upload `wp-nice-gallery` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Done
---
+++ wp-nice-gallery.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: WP Nice Gallery
+Version: 1.1
+Description: Replaces the default gallery function with a nicer one
+Author: Nico Hauser & Michael Ziörjen
+
+*/
+
+add_action('wp_enqueue_scripts', 'wp_nice_gallery_css');
+
+remove_shortcode('gallery');
+add_shortcode('gallery', 'wp_nice_gallery_func');
+
+add_action('wp_footer', 'wp_nice_gallery_footer_scripts');
+
+function wp_nice_gallery_func($atts){
+	
+	global $wp_nice_gallery_counter, $wp_nice_galleries;
+	
+	if(!isset($wp_nice_gallery_counter)){
+		$wp_nice_gallery_counter='a';
+	}
+	if(!isset($wp_nice_galleries) || !is_array($wp_nice_galleries)){
+		$wp_nice_galleries=array();
+	}
+	
+	extract( shortcode_atts( array(
+		/*'link' => '',*/
+		'ids' => '',
+		/*'orderby' => '',*/
+	), $atts ) );
+
+	if(!empty($ids)){
+		$ids_ = explode(",", $ids);
+
+		$code="<div class='row'>";
+		$first = true;
+		foreach ($ids_ as $id) {
+			$file = get_post($id);
+
+			$title = $file->post_excerpt;
+			$link = $file->guid;
+			
+			$code.='<div class="col-xs-6 col-md-3">
+			<a href="'.$link.'" class="thumbnail" data-imagelightbox="'.$wp_nice_gallery_counter.'"><img src="'.$link.'" alt="'.$title.'"/></a>
+			</div>';
+	    }
+		$code.= "</div>";
+		
+		$wp_nice_galleries[]=$wp_nice_gallery_counter;
+		
+		//add to $wp_nice_gallery_counter
+		$wp_nice_gallery_counter = ++$wp_nice_gallery_counter;
+		
+		
+
+		wp_reset_query();
+
+		return $code;
+	}
+
+}
+
+function wp_nice_gallery_css() {
+	wp_enqueue_script('lightbox-script', plugins_url('js/imagelightbox.min.js', __FILE__), array('jquery'), false, true);
+	wp_enqueue_style('lightbox-css', plugins_url('css/lightbox.css', __FILE__));
+}
+
+function wp_nice_gallery_footer_scripts(){
+	global $wp_nice_galleries;
+	
+	
+	echo "<script>jQuery(document).ready(function() {".
+		"var galleries=".json_encode($wp_nice_galleries).";".
+		"if(galleries!= null) {".
+			"for(var i=0;i<galleries.length;i++){".
+				"niceGallery(galleries[i]);".
+			"}".
+		"}".
+	"});".
+	"</script>";
+}
+
+?>
